0e151032c64ab5b3822f431cd2a61b7cec8de1a1
rigid-caress-wand <rigid-caress-wand@duck.com>
2026-02-09 12:29:29 +0530

    Update ipspoofing.cc

--- ipspoofing.cc
+++ ipspoofing.cc
@@ -7,38 +7,47 @@
 
 using namespace ns3;
 
+// SPOOFING DETECTION FUNCTION
 void IngressFilter (Ptr<const Packet> p, Ptr<Ipv4> ipv4, uint32_t interface) {
     Ipv4Header ip;
     p->PeekHeader (ip);
     Ipv4Address src = ip.GetSource ();
     Ipv4InterfaceAddress ifAddr = ipv4->GetAddress (interface, 0);
+    
+    // Check if Source IP matches the Interface Subnet
     if (src.CombineMask (ifAddr.GetMask ()) != ifAddr.GetLocal ().CombineMask (ifAddr.GetMask ())) {
         NS_LOG_UNCOND ("Spoofed packet detected from " << src);
     }
 }
 
 int main (int argc, char *argv[]) {
+    // 1. Create Nodes
     NodeContainer nodes;
     nodes.Create (2);
 
+    // 2. Configure Link
     PointToPointHelper p2p;
     p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
     p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
-
     NetDeviceContainer devices = p2p.Install (nodes);
 
+    // 3. Install Internet Stack
     InternetStackHelper stack;
     stack.Install (nodes);
 
+    // 4. Assign IP Addresses
     Ipv4AddressHelper address;
     address.SetBase ("10.1.1.0", "255.255.255.0");
     Ipv4InterfaceContainer interfaces = address.Assign (devices);
 
+    // 5. Populate Routing
     Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
 
+    // 6. CONNECT INGRESS FILTER (The Security Part)
     Ptr<Ipv4> ipv4 = nodes.Get(1)->GetObject<Ipv4>();
     ipv4->TraceConnectWithoutContext("Rx", MakeCallback(&IngressFilter));
 
+    // 7. Applications (UDP)
     uint16_t port = 9;
     OnOffHelper source ("ns3::UdpSocketFactory", InetSocketAddress (interfaces.GetAddress (1), port));
     source.SetAttribute ("DataRate", StringValue ("5Mbps"));
@@ -53,12 +62,15 @@ int main (int argc, char *argv[]) {
     sinkApps.Start (Seconds (0.0));
     sinkApps.Stop (Seconds (10.0));
 
+    // 8. Flow Monitor
     FlowMonitorHelper flowmon;
     Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
 
+    // 9. Run
     Simulator::Stop (Seconds (10.0));
     Simulator::Run ();
 
+    // 10. Stats
     monitor->CheckForLostPackets ();
     std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats ();
 
