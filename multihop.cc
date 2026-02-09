dcb35a527326ce1a3f372fea064e14da492c86db
rigid-caress-wand <rigid-caress-wand@duck.com>
2026-02-09 12:07:54 +0530

    Create multihop.cc

---
+++ multihop.cc
@@ -0,0 +1,64 @@
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/point-to-point-module.h"
+#include "ns3/applications-module.h"
+#include "ns3/flow-monitor-module.h"
+
+using namespace ns3;
+
+int main (int argc, char *argv[]) {
+    NodeContainer nodes;
+    nodes.Create (3);
+
+    PointToPointHelper p2p;
+    p2p.SetDeviceAttribute ("DataRate", StringValue ("10Mbps"));
+    p2p.SetChannelAttribute ("Delay", StringValue ("2ms"));
+
+    NetDeviceContainer d01 = p2p.Install (nodes.Get (0), nodes.Get (1));
+    NetDeviceContainer d12 = p2p.Install (nodes.Get (1), nodes.Get (2));
+
+    InternetStackHelper stack;
+    stack.Install (nodes);
+
+    Ipv4AddressHelper address;
+    address.SetBase ("10.1.1.0", "255.255.255.0");
+    Ipv4InterfaceContainer i01 = address.Assign (d01);
+
+    address.SetBase ("10.1.2.0", "255.255.255.0");
+    Ipv4InterfaceContainer i12 = address.Assign (d12);
+
+    Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
+
+    uint16_t port = 9;
+    OnOffHelper source ("ns3::UdpSocketFactory", InetSocketAddress (i12.GetAddress (1), port));
+    source.SetAttribute ("DataRate", StringValue ("5Mbps"));
+    source.SetAttribute ("PacketSize", UintegerValue (1024));
+
+    ApplicationContainer sourceApps = source.Install (nodes.Get (0));
+    sourceApps.Start (Seconds (1.0));
+    sourceApps.Stop (Seconds (10.0));
+
+    PacketSinkHelper sink ("ns3::UdpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), port));
+    ApplicationContainer sinkApps = sink.Install (nodes.Get (2));
+    sinkApps.Start (Seconds (0.0));
+    sinkApps.Stop (Seconds (10.0));
+
+    FlowMonitorHelper flowmon;
+    Ptr<FlowMonitor> monitor = flowmon.InstallAll ();
+
+    Simulator::Stop (Seconds (10.0));
+    Simulator::Run ();
+
+    monitor->CheckForLostPackets ();
+    std::map<FlowId, FlowMonitor::FlowStats> stats = monitor->GetFlowStats ();
+
+    for (auto it = stats.begin (); it != stats.end (); ++it) {
+        std::cout << "Flow " << it->first << " Lost: " << it->second.lostPackets << "\n";
+        std::cout << "Rx Bytes: " << it->second.rxBytes << "\n";
+        std::cout << "Delay: " << it->second.delaySum.GetSeconds() / it->second.rxPackets << "\n";
+    }
+
+    Simulator::Destroy ();
+    return 0;
+}
